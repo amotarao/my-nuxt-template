ffd7f826b8a02b641a38fae32fced78d8f80f1fc
Amon Sawamura <amon.sawamura@gmail.com>
2020-05-08 14:43:18 +0900

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,14 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
+  extends: ['@nuxtjs', 'prettier', 'prettier/vue', 'plugin:prettier/recommended', 'plugin:nuxt/recommended'],
+  plugins: ['prettier'],
   rules: {
-  }
-}
+    'vue/html-closing-bracket-newline': 'warn',
+  },
+};
